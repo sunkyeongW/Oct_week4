5e39a2f170ca656ad43b5c2c3b5158102f081008
sunkyeongW <bluelight0949@naver.com>
2022-10-23 13:40:48 +0900

    A database query statement was created and extracted.

---
+++ part_03/test_db_topic_backup.sql
@@ -0,0 +1,55 @@
+-- MariaDB dump 10.19  Distrib 10.4.25-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: test_db
+-- ------------------------------------------------------
+-- Server version	10.4.25-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `topic_backup`
+--
+
+DROP TABLE IF EXISTS `topic_backup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `topic_backup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `author` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `profile` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `topic_backup`
+--
+
+LOCK TABLES `topic_backup` WRITE;
+/*!40000 ALTER TABLE `topic_backup` DISABLE KEYS */;
+INSERT INTO `topic_backup` VALUES (1,'MySQL','MySQL is ...','2022-10-23 12:07:45','egoing','developer'),(2,'Oracle','ORACLE is ...','2022-10-23 12:10:39','egoing','developer'),(3,'SQL Server','SQL Server is ...','2022-10-23 12:11:25','duru','data administrator'),(4,'PostgreSQL','SQL PostgreSQL is ...','2022-10-23 12:12:31','taeho','data scientist, developer');
+/*!40000 ALTER TABLE `topic_backup` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-23 13:40:12
--- readme.md
+++ readme.md
@@ -30,3 +30,69 @@
 
 # pypi
     파이썬으로 만든 패키지들을 관리하고 열람할 수 있는 저장소.
+
+# DB의 목적
+    SQL(DB서버에 요청하기 위한 언어)이라는 컴퓨터 언어를 통해서 데이터 제어 가능.
+    DB의 데이터들을 앱이나 웹을 이용해 다른 유저에게도 공유 가능.
+
+# MySQL 접속
+    mysql -uroot -p : mysql 접속
+
+# MySQL 구조
+    표 < 데이터베이스(스키마) < 데이터베이스 서버
+    표(table) : 데이터를 기록하는 공간
+        -수평구조(row, 행)
+        -수직구조(column, 열)
+    데이터베이스 : 표를 그루핑한 곳
+        -데이터베이스(스키마)가 모여 데이터 베이스 서버 안에 저장.
+
+# MySQL 스키마(schema)
+    CREATE DATABASE DB이름 : DB 생성.
+    DROP DATABASE DB이름 : DB 삭제.
+    USE DB이름 : DB 선택.
+
+# MySQL의 표(table) 생성
+    CREATE TABLE [table이름] () : 테이블 생성.
+        -INT : 정수만 등록 가능
+        -NOT NULL : 빈칸 불가
+        -AUTO_INCREMENT : 값 자동 증가
+        -VARCHAR : 최대 글자 개수
+
+# MySQL의 CRUD
+    Create : INSERT INTO [테이블이름] () VALUES ()
+    Read : 
+        -생성된 데이터 출력 : SELECT * FROM [테이블 이름]
+        -특정 값만 출력 : WHERE [TABLE 행]='특정 값'
+        -정렬 : ORDER BY 원하는 정렬방식
+        -데이터 수 지정 : LIMIT
+    Update : UPDATE SET 행='수정값' WHERE ID값
+    Delete : DELETE FROM [테이블] WHERE ID값
+
+# 관계형 데이터베이스의 필요성
+    장점: 별도의 데이터 테이블을 중복을 방지하여 유지 보수가 쉬움.
+    단점: 별도의 표를 열어 비교해봐야 하기 때문에 직관적이지 않음.
+
+# MySQL JOIN
+    분리된 테이블을 읽을 때 통합해서 보여주기가 가능.
+    SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id;
+    
+    AS : 행을 바꾸고 싶을때 사용.
+
+# DATABASE SERVER
+인터넷 위에서 동작하는 컴퓨터들은 정보를 요청과 응답으로 주고 받음.
+    요청하는 곳 : client(클라이언트)
+    응답하는 곳 : server(서버)
+
+
+    데이터베이스 클라이언트 <-> 데이터베이스 서버
+        데이터베이스 클라이언트는 MySQL이라는 명령어 기반의 프로그램을 이용.
+
+# MySQL Client
+    MySQL monitor : 명령어 기반, 어디서든 사용할 수 있는 프로그램.
+    MySQL workbench : GUI 기반
+
+# MySQL workbench
+    
+
+
+    
